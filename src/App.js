011a3331b2eaf043ad148dacf9d22e2bff71e0ed
Thomas <thomas@craftacademy.se>
2019-10-05 11:42:01 +0200

    adds cypress and first integration tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import EmployeeList from './EmployeeList'
 
 const App = () => {
   return (
     <>
       <h1>Employee list</h1>
+      <section id="main">
+        <EmployeeList />
+      </section>
     </>
   );
 }
